74f2801479da096366f396c659cc23a12c448fdc
chenqiang <1429178451@qq.com>
2018-11-28 09:35:55 +0800

    结账单位管理API增加注释，字段大写改为小写

--- src/main/java/com/zero/egg/controller/SupplierManageController.java
+++ src/main/java/com/zero/egg/controller/SupplierManageController.java
@@ -2,10 +2,12 @@ package com.zero.egg.controller;
 
 import java.util.List;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -15,39 +17,24 @@ import com.zero.egg.tool.Message;
 import com.zero.egg.tool.UtilConstants;
 import com.zero.egg.tool.UtilConstants.ResponseCode;
 import com.zero.egg.tool.UtilConstants.ResponseMsg;
+
+@Api(value="合作单位管理")
 @RestController
-@RequestMapping("/SupplierManage")
+@RequestMapping("/suppliermanage")
 public class SupplierManageController {
 	@Autowired
     private SupplierService supplierService;
 	/**
 	 * @Description 新增合作单位  可分为供应商/合作商
-	 * @Param    strSupplierPerSon   姓名
-	 * @Param	  strSupplierPhone	  联系方式
-	 * @param    strSupplierKindCode  类型（SK01供应商/SK02 合作商）
-	 * @param	  strSupplierShortName  代号
-	 * @param    strSupplierTypeCode  合作单位种类（固定客户/散户/不稳定客户）
-	 * @param    strStoreCode         店铺编码
-	 * @param    strDesc			  备注信息
 	 * @Return   是否成功
-	 **/ 
-	
-	 @RequestMapping(value = "/AddSupplier",method = RequestMethod.POST)
-	 public Message AddSupplier(String strSupplierTypeCode, String strSupplierName,String strSupplierPerSon,String strSupplierPhone,String strSupplierKindCode,String strSupplierShortName,String strStoreCode,String strSupplierDesc,int LngState){	 
+	 **/
+	 @ApiOperation(value="新增合作单位")
+	 @RequestMapping(value = "/addsupplier",method = RequestMethod.POST)
+	 public Message AddSupplier(@RequestBody  Supplier model){
 		 	 Message message = new Message();
 			 try {  
 				 //店铺编码  实际根据界面传值，测试使用SC00001
-				  Supplier model=new Supplier();
-				  model.setStrSupplierName(strSupplierName);
-				  model.setStrSupplierKindCode(strSupplierKindCode);
-				  model.setStrSupplierPerSon(strSupplierPerSon);
-				  model.setStrSupplierPhone(strSupplierPhone);
-				  model.setStrSupplierShortName(strSupplierShortName);
-				  model.setStrSupplierDesc(strSupplierDesc);
-				  model.setStrSupplierTypeCode(strSupplierTypeCode);
-				  model.setStrStoreCode(strStoreCode);
-				  model.setLngState(LngState);
-				  int strval=supplierService.AddSupplier(model);
+				    int strval=supplierService.AddSupplier(model);
 		        	if (strval>0) {
 		        		 message.setState(UtilConstants.ResponseCode.SUCCESS_HEAD);
 		                 message.setMessage(UtilConstants.ResponseMsg.SUCCESS);
@@ -71,33 +58,15 @@ public class SupplierManageController {
 	 
 		/**
 		 * @Description 更新合作单位(删除共用，不作物理删除，更新状态为无效状态)  可分为供应商/合作商
-		 * @param    strSupplierCode     合作单位编码
-		 * @param	  strSupplierName     合作单位名称
-		 * @Param    strSupplierPerSon   姓名
-		 * @param    strSupplierTypeCode  合作单位种类（固定客户/散户/不稳定客户）
-		 * @Param	  strSupplierPhone	  联系方式
-		 * @param    strSupplierKindCode  类型（SK01供应商/SK02 合作商）
-		 * @param	  strSupplierShortName  代号
-		 * @param    strStoreCode         店铺编码
-		 * @param    strDesc			  备注信息
 		 * @Return   是否成功
-		 **/ 
-	 @RequestMapping(value = "/UpdateSupplier",method = RequestMethod.POST)
-	  public Message UpdateSupplier(String strSupplierTypeCode, String strSupplierCode, String strSupplierName,String strSupplierPerSon,String strSupplierPhone,String strSupplierKindCode,String strSupplierShortName,String strStoreCode,String strSupplierDesc,int LngState) {
+		 **/
+		@ApiOperation(value="修改合作单位信息",notes="合作单位编码不能为空")
+	  @RequestMapping(value = "/updatesupplier",method = RequestMethod.POST)
+	  public Message UpdateSupplier(@RequestBody  Supplier model) {
 		 Message message = new Message();
 		 try {  
 			 //店铺编码  实际根据界面传值，测试使用SC00001
-			  Supplier model=new Supplier();
-			  model.setStrSupplierCode(strSupplierCode);
-			  model.setStrSupplierName(strSupplierName);
-			  model.setStrSupplierKindCode(strSupplierKindCode);
-			  model.setStrSupplierPerSon(strSupplierPerSon);
-			  model.setStrSupplierPhone(strSupplierPhone);
-			  model.setStrSupplierShortName(strSupplierShortName);
-			  model.setStrSupplierDesc(strSupplierDesc);
-			  model.setStrStoreCode(strStoreCode);
-			  model.setLngState(LngState);
-			  int strval=supplierService.UpdateSupplier(model);
+			    int strval=supplierService.UpdateSupplier(model);
 	        	if (strval>0) {
 	        		 message.setState(UtilConstants.ResponseCode.SUCCESS_HEAD);
 	                 message.setMessage(UtilConstants.ResponseMsg.SUCCESS);
@@ -118,28 +87,17 @@ public class SupplierManageController {
 		 	}
 		
 	  }
-	 
-	 
-	 @RequestMapping(value = "/GetSupplierList",method = RequestMethod.POST)
-	 public Message GetSupplierList(int pageNum,int pageSize,String strStoreCode, String strSupplierCode, String strSupplierKindCode, String strSupplierShortName,String strSupplierTypeCode,Integer  LngState ) {
+
+	@ApiOperation(value="查询合作单位列表",notes="分页查询，各种条件查询")
+	@ApiImplicitParams({
+			@ApiImplicitParam(paramType="query",name="页码",value="pageNum",dataType="int"),
+			@ApiImplicitParam(paramType="query",name="页大小",value="pageSize",dataType="int")
+	})
+	 @RequestMapping(value = "/getsupplierlist",method = RequestMethod.POST)
+	 public Message GetSupplierList(@RequestParam int pageNum,@RequestParam int pageSize, @RequestBody  Supplier model) {
 		  Message ms = new Message();
-		  Supplier model=new Supplier();
-		  //合作单位编码
-		  model.setStrSupplierCode(strSupplierCode);
-		  //合作单位类型: 上游供应商   下游合作商  根据页面对应固定写死
-		  model.setStrSupplierKindCode(strSupplierKindCode);
-		  //合作单位代号（简称）
-		  model.setStrSupplierShortName(strSupplierShortName);
-		  //下游合作商类型（固定客户、散户）  上游供应商无此属性
-		  model.setStrSupplierTypeCode(strSupplierTypeCode);
-		  //合作单位所属店铺
-		  model.setStrStoreCode(strStoreCode);
-		  //状态
-		  model.setLngState(LngState);
-		  
 		  PageHelper.startPage(pageNum, pageSize);
 		  List<Supplier> Supplier=supplierService.GetSupplierList(model);
-		  
 		  PageInfo<Supplier> pageInfo = new PageInfo<>(Supplier);
 		  ms.setData(pageInfo);
 		  ms.setState(ResponseCode.SUCCESS_HEAD);
