ce5d0a87d76e4bdc2f3f37f5c53784b2374b03cc
JC <358632079@qq.com>
2018-12-10 22:48:14 +0800

    跨域问题

--- src/main/java/com/zero/egg/controller/WebConfigurer.java
+++ src/main/java/com/zero/egg/controller/WebConfigurer.java
@@ -3,6 +3,7 @@ package com.zero.egg.controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,15 +11,37 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfigurer implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		//设置允许跨域的路径
-        registry.addMapping("/**")
-                //设置允许跨域请求的域名
-                .allowedOrigins("*")
-                //是否允许证书 不再默认开启
-                .allowCredentials(true)
-                //设置允许的方法
-                .allowedMethods("*")
-                //跨域允许时间
-                .maxAge(3600);
+		// 设置允许跨域的路径
+		registry.addMapping("/**")
+				// 设置允许跨域请求的域名
+				.allowedOrigins("*")
+				// 是否允许证书 不再默认开启
+				.allowCredentials(true)
+				// 设置允许的方法
+				.allowedMethods("*")
+				// 跨域允许时间
+				.maxAge(3600);
+	}
+
+	/**
+	 * 添加静态资源--过滤swagger-api (开源的在线API文档)
+	 * 
+	 * @param registry
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 过滤swagger
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+		registry.addResourceHandler("/swagger-resources/**")
+				.addResourceLocations("classpath:/META-INF/resources/swagger-resources/");
+
+		registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/META-INF/resources/swagger*");
+
+		registry.addResourceHandler("/v2/api-docs/**")
+				.addResourceLocations("classpath:/META-INF/resources/v2/api-docs/");
+
 	}
 }
