790183aade10830886421c5e6867f265a3dce0c2
Nero_Li <375892799@qq.com>
2019-05-04 17:07:37 +0800

    # 打印二维码，简化前端传参;二维码信息加密,后台提供解码解密工具;新增二维码查重,同一企业同一店铺同一供应商的同一产品类别只允许生成一张母二维码(补充提交)

---
+++ src/main/java/com/zero/egg/requestDTO/BarCodeListRequestDTO.java
@@ -0,0 +1,31 @@
+package com.zero.egg.requestDTO;
+
+import com.zero.egg.tool.PageDTO;
+import lombok.Data;
+
+/**
+ * 二维码列表查询
+ *
+ * @ClassName BarCodeListRequestDTO
+ * @Author lyming
+ * @Date 2019/5/4 11:38
+ **/
+@Data
+public class BarCodeListRequestDTO extends PageDTO {
+
+    /**
+     * 供应商名字关键字(模糊查询)
+     */
+    private String supplierName;
+
+    /**
+     * 企业id
+     */
+    private String companyId;
+
+    /**
+     * 店铺id
+     */
+    private String shopId;
+
+}
