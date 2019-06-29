d9300bf7729bb296bd621a0500963bef4c3fd342
1429178451@qq.com <qq615528>
2019-06-29 13:02:43 +0800

    修改接口

--- src/main/java/com/zero/egg/controller/UserController.java
+++ src/main/java/com/zero/egg/controller/UserController.java
@@ -9,6 +9,7 @@ import com.zero.egg.requestDTO.LoginUser;
 import com.zero.egg.requestDTO.UserRequest;
 import com.zero.egg.service.IUserService;
 import com.zero.egg.tool.Message;
+import com.zero.egg.tool.StringTool;
 import com.zero.egg.tool.TransferUtil;
 import com.zero.egg.tool.UtilConstants;
 import io.swagger.annotations.Api;
@@ -21,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -38,26 +41,26 @@ public class UserController {
 
 	@Autowired
 	private IUserService userService;
-	
+
 	@LoginToken
-	@ApiOperation(value="分页查询员工")
-	@RequestMapping(value="/list.data",method=RequestMethod.POST)
+	@ApiOperation(value = "分页查询员工")
+	@RequestMapping(value = "/list.data", method = RequestMethod.POST)
 	public Message<IPage<User>> list(
-			@RequestBody @ApiParam(required=false,name="userRequest",value="查询字段：可选（名称 、编号、状态）") UserRequest userRequest
-			,HttpServletRequest request) {
+			@RequestBody @ApiParam(required = false, name = "userRequest", value = "查询字段：可选（名称 、编号、状态）") UserRequest userRequest
+			, HttpServletRequest request) {
 		//ListResponse<User> response = new ListResponse<>(ApiConstants.ResponseCode.EXECUTE_ERROR, ApiConstants.ResponseMsg.EXECUTE_ERROR);
 		Message<IPage<User>> message = new Message<IPage<User>>();
 		//当前登录用户
 		LoginUser loginUser = (LoginUser) request.getAttribute(ApiConstants.LOGIN_USER);
 		message = userService.listPage(userRequest, loginUser);
 		return message;
-		
+
 	}
-	
+
 	@LoginToken
-	@ApiOperation(value="根据Id查询员工")
-	@RequestMapping(value="/get.data",method=RequestMethod.POST)
-	public Message<User> getById(@RequestBody @ApiParam(required=false,name="userRequest",value="员工主键") UserRequest userRequest) {
+	@ApiOperation(value = "根据Id查询员工")
+	@RequestMapping(value = "/get.data", method = RequestMethod.POST)
+	public Message<User> getById(@RequestBody @ApiParam(required = false, name = "userRequest", value = "员工主键") UserRequest userRequest) {
 		//BaseResponse<Object> response = new BaseResponse<>(ApiConstants.ResponseCode.EXECUTE_ERROR, ApiConstants.ResponseMsg.EXECUTE_ERROR);
 		Message<User> message = new Message<User>();
 		User user = userService.getById(userRequest.getId());
@@ -65,19 +68,19 @@ public class UserController {
 			message.setState(UtilConstants.ResponseCode.SUCCESS_HEAD);
 			message.setMessage(UtilConstants.ResponseMsg.SUCCESS);
 			message.setData(user);
-		}else {
+		} else {
 			message.setState(UtilConstants.ResponseCode.SUCCESS_HEAD);
 			message.setMessage(UtilConstants.ResponseMsg.SUCCESS);
 		}
 		return message;
 	}
-	
+
 	@LoginToken
-	@ApiOperation(value="根据条件查询员工")
-	@RequestMapping(value="/get-condition.data",method=RequestMethod.POST)
+	@ApiOperation(value = "根据条件查询员工")
+	@RequestMapping(value = "/get-condition.data", method = RequestMethod.POST)
 	public Message<List<User>> getByCompanyId(
-			@RequestBody @ApiParam(required=false,name="user",value="查询字段：可选（名称 、编号、状态）") UserRequest user
-			,HttpServletRequest request) {
+			@RequestBody @ApiParam(required = false, name = "user", value = "查询字段：可选（名称 、编号、状态）") UserRequest user
+			, HttpServletRequest request) {
 		//BaseResponse<Object> response = new BaseResponse<>(ApiConstants.ResponseCode.EXECUTE_ERROR, ApiConstants.ResponseMsg.EXECUTE_ERROR);
 		Message<List<User>> message = new Message<List<User>>();
 		//当前登录用户
@@ -85,15 +88,15 @@ public class UserController {
 		message = userService.listAll(user, loginUser);
 		return message;
 	}
-	
-	
+
+
 	@LoginToken
 	//@PassToken
-	@ApiOperation(value="新增员工")
-	@RequestMapping(value="/add.do",method=RequestMethod.POST)
+	@ApiOperation(value = "新增员工")
+	@RequestMapping(value = "/add.do", method = RequestMethod.POST)
 	public Message<Object> add(
-			@RequestBody @ApiParam(required=true,name="user",value="员工信息:（必填）编号，（必填）登录名,（必填）名称，（必填）电话，（必填）性别，（选填）密码(不填则默认888888)") User user
-			,HttpServletRequest request) {
+			@RequestBody @ApiParam(required = true, name = "user", value = "员工信息:（必填）编号，（必填）登录名,（必填）名称，（必填）电话，（必填）性别，（选填）密码(不填则默认888888)") User user
+			, HttpServletRequest request) {
 		//BaseResponse<Object> response = new BaseResponse<>(ApiConstants.ResponseCode.EXECUTE_ERROR, ApiConstants.ResponseMsg.EXECUTE_ERROR);
 		Message<Object> message = new Message<Object>();
 		//当前登录用户
@@ -101,55 +104,76 @@ public class UserController {
 		message = userService.save(user, loginUser);
 		return message;
 	}
-	
+
 	@LoginToken
-	@ApiOperation(value="根据id修改员工信息")
-	@RequestMapping(value="/edit.do",method=RequestMethod.POST)
+	@ApiOperation(value = "根据id修改员工信息")
+	@RequestMapping(value = "/edit.do", method = RequestMethod.POST)
 	public Message<Object> edit(
-			@RequestBody  @ApiParam(required=true,name="user",value="员工信息:（必填）员工主键，（选填）编号，（选填）名称，（选填）电话，（选填）性别") User user
-			,HttpServletRequest request) {
+			@RequestBody @ApiParam(required = true, name = "user", value = "员工信息:（必填）员工主键，（选填）编号，（选填）名称，（选填）电话，（选填）性别") User user
+			, HttpServletRequest request) {
 		//BaseResponse<Object> response = new BaseResponse<>(ApiConstants.ResponseCode.EXECUTE_ERROR, ApiConstants.ResponseMsg.EXECUTE_ERROR);
 		Message<Object> message = new Message<Object>();
 		LoginUser loginUser = (LoginUser) request.getAttribute(ApiConstants.LOGIN_USER);
-		message =userService.updateById(user, loginUser);
+		message = userService.updateById(user, loginUser);
 		return message;
 	}
-	
+
 	@LoginToken
-	@ApiOperation(value="员工离职")
-	@RequestMapping(value="/user-dimission.do",method=RequestMethod.POST)
-	public Message<Object> edit(HttpServletRequest request,@RequestBody @ApiParam(required=false,name="userRequest",value="员工主键") UserRequest userRequest) {
+	@ApiOperation(value = "员工离职")
+	@RequestMapping(value = "/user-dimission.do", method = RequestMethod.POST)
+	public Message<Object> edit(HttpServletRequest request, @RequestBody @ApiParam(required = false, name = "userRequest", value = "员工主键") UserRequest userRequest) {
 		//BaseResponse<Object> response = new BaseResponse<>(ApiConstants.ResponseCode.EXECUTE_ERROR, ApiConstants.ResponseMsg.EXECUTE_ERROR);
 		Message<Object> message = new Message<Object>();
 		LoginUser loginUser = (LoginUser) request.getAttribute(ApiConstants.LOGIN_USER);
 		User user = new User();
-		TransferUtil.copyProperties(user,userRequest);
-		message =userService.updateById(user, loginUser);
+		TransferUtil.copyProperties(user, userRequest);
+		message = userService.updateById(user, loginUser);
 		return message;
 	}
-	
+
 	@LoginToken
 	//@PassToken
-	@ApiOperation(value="根据id删除员工信息")
-	@RequestMapping(value="/del.do",method=RequestMethod.POST)
+	@ApiOperation(value = "根据id删除员工信息")
+	@RequestMapping(value = "/del.do", method = RequestMethod.POST)
 	public Message<Object> del(HttpServletRequest request,
-			@RequestBody @ApiParam(required=false,name="userRequest",value="员工主键") UserRequest userRequest) {
+							   @RequestBody @ApiParam(required = false, name = "userRequest", value = "员工主键") UserRequest userRequest) {
 		//BaseResponse<Object> response = new BaseResponse<>(ApiConstants.ResponseCode.EXECUTE_ERROR, ApiConstants.ResponseMsg.EXECUTE_ERROR);
 		Message<Object> message = new Message<Object>();
 		LoginUser loginUser = (LoginUser) request.getAttribute(ApiConstants.LOGIN_USER);
-		message =userService.deleteById(userRequest.getId(),loginUser);
+		message = userService.deleteById(userRequest.getId(), loginUser);
 		return message;
 	}
-	
+
 	@LoginToken
-	@ApiOperation(value="批量删除员工信息")
-	@RequestMapping(value="/batchdel.do",method=RequestMethod.POST)
+	@ApiOperation(value = "批量删除员工信息")
+	@RequestMapping(value = "/batchdel.do", method = RequestMethod.POST)
 	public Message<Object> batchDel(HttpServletRequest request,
-			@RequestBody @ApiParam(required=false,name="userRequest",value="员工主键字符串") UserRequest userRequest) {
+									@RequestBody @ApiParam(required = false, name = "userRequest", value = "员工主键字符串") UserRequest userRequest) {
 		//BaseResponse<Object> response = new BaseResponse<>(ApiConstants.ResponseCode.EXECUTE_ERROR, ApiConstants.ResponseMsg.EXECUTE_ERROR);
 		Message<Object> message = new Message<Object>();
 		LoginUser loginUser = (LoginUser) request.getAttribute(ApiConstants.LOGIN_USER);
-		message =userService.deleteBatchById(userRequest.getIds(), loginUser);
+
+		List<String> idsList = StringTool.splitToList(userRequest.getIds(), ",");
+		if (idsList != null) {
+			List<User> userList = new ArrayList<>();
+			for (String id : idsList) {
+				User shop = new User();
+				shop.setDr(true);
+				shop.setId(id);
+				shop.setModifier(loginUser.getId());
+				shop.setModifytime(new Date());
+				userList.add(shop);
+			}
+			if (userService.updateBatchById(userList)) {//逻辑删除
+				message.setState(UtilConstants.ResponseCode.SUCCESS_HEAD);
+				message.setMessage(UtilConstants.ResponseMsg.SUCCESS);
+			} else {
+				message.setState(UtilConstants.ResponseCode.EXCEPTION_HEAD);
+				message.setMessage(UtilConstants.ResponseMsg.FAILED);
+			}
+
+
+		}
 		return message;
 	}
 }
