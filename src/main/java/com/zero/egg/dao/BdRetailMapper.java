c4b5ab3fc84c4544d5c35534fa4f137b23deb3a6
Nero_Li <375892799@qq.com>
2019-08-01 15:34:58 +0800

    # 接口开发:查询零售记录,新增参数验证工具类 补充提交

---
+++ src/main/java/com/zero/egg/dao/BdRetailMapper.java
@@ -0,0 +1,8 @@
+package com.zero.egg.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zero.egg.model.BdRetail;
+
+public interface BdRetailMapper extends BaseMapper<BdRetail> {
+
+}
