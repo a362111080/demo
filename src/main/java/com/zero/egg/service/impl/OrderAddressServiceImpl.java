f6f33e2d3096ed7a195aec29612d35c8c4f1db0d
Nero_Li <375892799@qq.com>
2019-09-28 14:52:20 +0800

    # 地址管理bug fix3

--- src/main/java/com/zero/egg/service/impl/OrderAddressServiceImpl.java
+++ src/main/java/com/zero/egg/service/impl/OrderAddressServiceImpl.java
@@ -61,7 +61,7 @@ public class OrderAddressServiceImpl implements OrderAddressService {
             TransferUtil.copyProperties(orderAddress, orderAddressDTO);
             if (orderAddress.getIsDefault()) {
                 orderAddressMapper.update(new OrderAddress(), new UpdateWrapper<OrderAddress>()
-                        .eq("use_id", orderAddress.getUserId()));
+                        .eq("user_id", orderAddress.getUserId()));
             }
             orderAddressMapper.update(orderAddress, new UpdateWrapper<OrderAddress>()
                     .eq("id", orderAddressDTO.getId())
