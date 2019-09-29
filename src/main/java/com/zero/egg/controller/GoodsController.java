d1a2bd808a8016a6c03f99059c40b7b7f454133f
Nero_Li <375892799@qq.com>
2019-09-29 16:29:32 +0800

    # 上传多张图片测试接口

--- src/main/java/com/zero/egg/controller/GoodsController.java
+++ src/main/java/com/zero/egg/controller/GoodsController.java
@@ -15,11 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -69,10 +72,50 @@ public class GoodsController {
         return message;
     }
 
+    @RequestMapping(value = "/uploadpics", method = RequestMethod.POST)
+    @ResponseBody
+    @LoginToken
+    public Message uploadpics(HttpServletRequest request) {
+        LoginUser user = (LoginUser) request.getAttribute(ApiConstants.LOGIN_USER);
+        Message message = new Message();
+        ImageHolder thumbnail = null;
+        String thumbnailAddr;
+        //从session中获取servletContext,相当于tomcat容器了,然后转换成spring的CommonsMultipartResolver
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        try {
+            List<String> addrs = new ArrayList<>();
+            //如果CommonsMultipartResolver里面有文件
+            if (commonsMultipartResolver.isMultipart(request)) {
+                //将servlet中的request转换成spring中的MultipartHttpServletRequest(spring)
+                MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+                //取出缩略图并构建ImageHolder对象,从MultipartHttpServletRequest中
+                List<MultipartFile> thumbnails = multipartHttpServletRequest.getFiles("thumbnail");
+                if (thumbnails.size() > 0) {
+                    for (MultipartFile thumbnailFile : thumbnails) {
+                        thumbnail = new ImageHolder(thumbnailFile.getOriginalFilename(), thumbnailFile.getInputStream());
+                        //如果商品缩略图不为null,则添加
+                        if (thumbnail != null && null != thumbnail.getImage()) {
+                            thumbnailAddr = addThumbnail(user, thumbnail);
+                            addrs.add(thumbnailAddr);
+                        }
+                    }
+                }
+                message.setData(addrs);
+                message.setState(UtilConstants.ResponseCode.SUCCESS_HEAD);
+                message.setMessage(UtilConstants.ResponseMsg.SUCCESS);
+            }
+        } catch (
+                Exception e) {
+            log.error("uploadpic" + e);
+            throw new ServiceException("uploadpic exception");
+        }
+        return message;
+    }
+
     /**
      * 生成缩略图
      *
-     * @param loginUser     当前登录用户
+     * @param loginUser   当前登录用户
      * @param imageHolder 图片处理的封装类
      */
     private String addThumbnail(LoginUser loginUser, ImageHolder imageHolder) {
