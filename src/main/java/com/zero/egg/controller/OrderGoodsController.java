77b20ddb0cac63346a51ecc53074b3e47bdfc61f
Nero_Li <375892799@qq.com>
2019-09-29 15:09:58 +0800

    # 订货平台加入独立的类目查询接口

--- src/main/java/com/zero/egg/controller/OrderGoodsController.java
+++ src/main/java/com/zero/egg/controller/OrderGoodsController.java
@@ -1,8 +1,12 @@
 package com.zero.egg.controller;
 
+import com.zero.egg.annotation.LoginToken;
 import com.zero.egg.api.ApiConstants;
+import com.zero.egg.model.OrderCategory;
 import com.zero.egg.requestDTO.LoginUser;
 import com.zero.egg.requestDTO.OrderGoodsRequestDTO;
+import com.zero.egg.responseDTO.OrderCategoryResponseDTO;
+import com.zero.egg.service.IShopService;
 import com.zero.egg.service.OrderGoodsService;
 import com.zero.egg.tool.Message;
 import com.zero.egg.tool.ServiceException;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @ClassName OrderGoodsController
@@ -33,6 +38,8 @@ public class OrderGoodsController {
     @Autowired
     HttpServletRequest request;
 
+    @Autowired
+    private IShopService shopService;
     @Autowired
     private OrderGoodsService orderGoodsService;
 
@@ -58,5 +65,37 @@ public class OrderGoodsController {
             msg.setMessage((UtilConstants.ResponseMsg.FAILED));
         }
         return msg;
+    }
+
+    @LoginToken
+    @ApiOperation(value = "查询类目")
+    @RequestMapping(value = "/queryordercategory", method = RequestMethod.POST)
+    public Message<Object> queryordercategory(@RequestBody OrderCategory model) {
+        Message<Object> message = new Message<Object>();
+        //当前登录用户
+        LoginUser loginUser = (LoginUser) request.getAttribute(ApiConstants.LOGIN_USER);
+        //TODO 校验登录用户绑定的店铺id是否包含model里面的id
+        if (model == null || null == model.getShopId()) {
+            message.setState(UtilConstants.ResponseCode.EXCEPTION_HEAD);
+            message.setMessage(UtilConstants.ResponseMsg.PARAM_MISSING);
+            return message;
+        }
+        List<OrderCategoryResponseDTO> ResponseDTO = shopService.GetOrderCateGory(model);
+        if (ResponseDTO.size() > 0) {
+            if (model.getIsQueryChild() == true) {
+                for (int m = 0; m < ResponseDTO.size(); m++) {
+                    model.setPid(ResponseDTO.get(m).getId());
+                    List<OrderCategory> child = shopService.GetOrderCateGoryChild(model);
+                    ResponseDTO.get(m).setOrderCategoryList(child);
+                }
+            }
+            message.setData(ResponseDTO);
+            message.setState(UtilConstants.ResponseCode.SUCCESS_HEAD);
+            message.setMessage(UtilConstants.ResponseMsg.SUCCESS);
+        } else {
+            message.setState(UtilConstants.ResponseCode.EXCEPTION_HEAD);
+            message.setMessage("店铺无商品分类信息");
+        }
+        return message;
     }
 }
